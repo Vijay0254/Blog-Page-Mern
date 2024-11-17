3e68718fa25266d096afc1f34ea44ff74991eb9d
Vijay0254 <140953246+Vijay0254@users.noreply.github.com>
2024-11-17 18:29:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.use('/auth', authRouter)
