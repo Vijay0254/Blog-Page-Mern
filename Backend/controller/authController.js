7c0a3f2ba2798e0732060f660b86de8363c375a4
Vijay0254 <140953246+Vijay0254@users.noreply.github.com>
2024-11-17 17:19:41 +0530

    Update authController.js

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -36,8 +36,8 @@ const loginController = async(req,res) =>{
             console.log(verifyPassword)
             if(verifyPassword){
                 const token = jwt.sign({email: exist.email, username: exist.username}, process.env.SECRET_KEY, {expiresIn: "1d"})
-                await res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'Strict' })
-                console.log("token:", token)
+                const tok = await res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'Strict' })
+                console.log("token:", tok)
                 return res.status(200).json({message: "User exist"})
             }
             else{
