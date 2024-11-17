af3c0b40608cdec45500581f0c8e747369377a21
Vijay0254 <140953246+Vijay0254@users.noreply.github.com>
2024-11-17 17:13:18 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -13,6 +13,8 @@ const Login = () => {
         event.preventDefault()
         try{
             const response = await axios.post(`${API_URL}/auth/login`,{email: email, password: password}, {withCredentials: true})
+            console.log(response)
+            console.log(response.data.message)
             if(response.data.message == "User exist"){
                 setemail("")
                 setpassword("")
